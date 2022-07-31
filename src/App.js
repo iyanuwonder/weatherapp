b8f2b53cbc7108ed3105445688c8f7c90a7caa9e
iyanuwonder <iyanumobolaji92@gmail.com>
2022-07-31 16:50:26 +0100

    First commit-weather app home page with an image background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios'
 
 function App() {
+
+  //const url ='http://api.openweathermap.org/data/2.5/weather?q=dallas&appid=5a7a18701e94fe9a1d62841a9e2838a4'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>dallas</p>
+          </div>
+          <div className="temp">
+            <h1>65F </h1>
+          </div>
+          <div className="description">
+            <p>clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            65F
+          </div>
+          <div className="humidity">
+            <p>25%</p>
+          </div>
+          <div className="wind">
+            12 MPH
+          </div>
+        </div>
+        
+      
+      </div>   
+
+
+
     </div>
   );
 }
